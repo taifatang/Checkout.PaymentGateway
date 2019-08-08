38ee9be2f98cb368f246634a4979baf1ad82f561
Tai Fa Tang <taifatang@hotmail.com>
2019-08-09 00:41:49 +0100

    add contract validation attribute and stub acquirer

---
+++ Src/Checkout.PaymentGateway.Host/AcquiringBank/TestCardAcquirerHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Checkout.PaymentGateway.Host.Processor;
+
+namespace Checkout.PaymentGateway.Host.AcquiringBank
+{
+    public class TestCardAcquirerHandler : IAcquirerHandler
+    {
+        private const string TestActivationCardNumber = "4242424242424242";
+        private readonly Dictionary<string, string> _mockResponse = new Dictionary<string, string>()
+        {
+            { "111", "Success" },
+            { "222", "Failed" }
+        };
+        private readonly IAcquirerHandler _innerAcquirerHandler;
+
+        public TestCardAcquirerHandler(IAcquirerHandler innerAcquirerHandler)
+        {
+            _innerAcquirerHandler = innerAcquirerHandler;
+        }
+
+        public AcquirerResponse Process(PaymentParameters request)
+        {
+            if (request.PaymentDetails.CardNumber == TestActivationCardNumber && _mockResponse.ContainsKey(request.PaymentDetails.SecurityCode))
+            {
+                return new AcquirerResponse()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Status = _mockResponse[request.PaymentDetails.SecurityCode]
+                };
+            }
+            return _innerAcquirerHandler.Process(request);
+        }
+    }
+}
