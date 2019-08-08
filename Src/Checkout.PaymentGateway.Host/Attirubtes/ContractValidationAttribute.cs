d954ea7d9106d4c937a11df70f384bc8739da6c6
Tai Fa Tang <taifatang@hotmail.com>
2019-08-09 00:41:49 +0100

    add contract validation attribute and stub acquirer

---
+++ Src/Checkout.PaymentGateway.Host/Attirubtes/ContractValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Checkout.PaymentGateway.Host.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Checkout.PaymentGateway.Host.Attirubtes
+{
+    public class ContractValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+
+                var response = new BaseResponse()
+                {
+                    Errors = context.ModelState.Select(x => new Error()
+                    {
+                        Code = x.Key,
+                        Details = string.Join(", ", x.Value.Errors.Select(y => y.ErrorMessage))
+                    })
+                };
+                context.Result = new BadRequestObjectResult(response);
+            }
+        }
+    }
+}
