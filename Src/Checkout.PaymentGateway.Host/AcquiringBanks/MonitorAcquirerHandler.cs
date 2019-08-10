69a1d4dec59ee44994fd2e6af3e27a3737d730aa
Tai Fa Tang <taifatang@hotmail.com>
2019-08-10 11:39:12 +0100

    add monitoring that's not really monitoring

---
+++ Src/Checkout.PaymentGateway.Host/AcquiringBanks/MonitorAcquirerHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Checkout.PaymentGateway.Host.Contracts.Acquirers;
+
+namespace Checkout.PaymentGateway.Host.AcquiringBanks
+{
+    //Not a requirement - New Relic monitoring
+    //New Relic already capture a lot of information including endpoint, response, duration etc. 
+    public class MonitorAcquirerHandler : IAcquirerHandler
+    {
+        private readonly IAcquirerHandler _inner;
+        //private readonly NewRelicMonitor _monitor;
+        public MonitorAcquirerHandler(IAcquirerHandler inner)
+        {
+            _inner = inner;
+        }
+
+        public async Task<AcquirerResponse> ProcessAsync(AcquirerRequest request)
+        {
+            //_monitor.AddParameter(request.PaymentOperation);
+            //_monitor.AddParameter(request.DesignatedAccount);
+            //_monitor.AddParameter(request.CurrencyCode);
+            //_monitor.AddParameter(request.Amount);
+            //_monitor.AddParameter(request.CardDetails.CardNumber);
+            //_monitor.AddParameter(request.CardDetails.ExpiryDate);
+
+            AcquirerResponse acquirerResponse;
+
+            //using (_monitor.Timer(nameof(IAcquirerHandler)))
+            //{
+            acquirerResponse = await _inner.ProcessAsync(request);
+            //}
+
+            //_monitor.AddParameter(_acquirerResponse.Id);
+            //_monitor.AddParameter(_acquirerResponse.Status);
+
+            return acquirerResponse;
+        }
+    }
+}
